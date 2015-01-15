51744badca5e2cc7de58856a055dff5584abb103
Jonah Karpinski <Jkarpinski14@yahoo.com>
2015-01-14 20:29:01 -0800

    Lecture 5 (01/14/15) completed

---
+++ construct.php
@@ -0,0 +1,47 @@
+<?php
+class Dance {
+	public $progressive;
+	public $dream;
+	public $hard;
+	public $trance;
+	public $tool;
+	public $trap;
+	public $dubstep;
+
+	function __construct($progressive, $dream, $hard, $trance, $tool, $trap, $dupstep) {
+		$this->progressive = $progressive;
+		$this->dream = $dream;
+		$this->hard = $hard;
+		$this->trance = $trance;
+		$this->tool = $tool;
+		$this->trap = $trap;
+		$this->dubstep = $dubstep;
+	}
+
+	function getName() {
+		return “Divisions of house are “ . $this->progressive . “ and” . $this->hard . “ and “ . $this->tool . “ .”;
+	}
+}
+
+class House extends Dance {
+	function __construct($progressive, $dream, $hard, $trance, $tool){
+		parent::__construct($progressive, $dream, $hard, $trance);
+		$this->tool = $tool;
+	}
+	function greet() {
+		return $this->tool;
+	}	
+}
+
+class Rave extends Dance {
+	function __construct($trance, $trap, $dubstep) {
+		parent::__construct($trance, $trap);
+		$this->dubstep = $dubstep;
+	}
+	function hello() {
+		return $this->dubstep;
+	}
+}
+
+$house = new House(“Tiesto”, “Rob Miles”, “w&w”, “Armin van Buuren”);
+print “Genre 1 is a ” . $house->getName();
